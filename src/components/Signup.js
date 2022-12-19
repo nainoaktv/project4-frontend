3b55140ea63ff201432aafb3f21cf6d3eb2811ae
nainoktv <nainoaktvillegas@gmail.com>
2022-12-18 18:12:16 -0800

    feat: update signup fields, user create redirect, handles, setStates etc.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,10 @@
 // Imports
-import * as React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -16,18 +12,60 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Redirect } from 'react-router-dom';
+
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const theme = createTheme();
 
 export default function Signup() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  
+  const [name, setName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const handleName = (e) => {
+		setName(e.target.value);
+	}
+
+  const handleUserName = (e) => {
+		setUserName(e.target.value);
+	}
+
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+	}
+
+	const handlePassword = (e) => {
+		setPassword(e.target.value);
+	}
+
+	const handleConfirmPassword = (e) => {
+		setConfirmPassword(e.target.value);
+	}
+
+  const handleSubmit = (e) => {
+		e.preventDefault(); 
+
+		if (password === confirmPassword && password.length >= 8) {
+			const newUser = { name, email, password };
+			axios.post(`${REACT_APP_SERVER_URL}/users/register`, newUser)
+				.then(response => {
+					console.log('===> Yay, new user');
+					console.log(response);
+					setRedirect(true);
+				})
+				.catch(error => console.log('===> Error in Signup', error));
+		} else {
+			if (password !== confirmPassword) return alert('Passwords don\'t match');
+			alert('Password needs to be at least 8 characters. Please try again.');
+		}
+	}
+
+  if (redirect) return <Redirect to="/profile" />
 
   return (
     <ThemeProvider theme={theme}>
@@ -49,50 +87,32 @@ export default function Signup() {
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+
+              <Grid item xs={12}>
                 <TextField
                   autoComplete="given-name"
-                  name="firstName"
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={handleName}
                   required
                   fullWidth
-                  id="firstName"
-                  label="First Name"
+                  id="fullName"
+                  label="Full Name"
                   autoFocus
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
+
               <Grid item xs={12}>
                 <TextField
                   required
                   fullWidth
+                  type="text"
                   id="displayName"
                   label="Username"
                   name="displayName"
                   autoComplete="user-name"
                 />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl required>
-                  <FormLabel id="genderHeader">Gender</FormLabel>
-                  <RadioGroup
-                    row
-                    aria-labelledby="demo-row-radio-buttons-group-label"
-                    name="row-radio-buttons-group"
-                  >
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                    <FormControlLabel value="other" control={<Radio />} label="Other" />
-                  </RadioGroup>
-                </FormControl>
               </Grid>
               <Grid item xs={12}>
                 <TextField
@@ -101,15 +121,27 @@ export default function Signup() {
                   id="email"
                   label="Email Address"
                   name="email"
+                  type="email"
                   autoComplete="email"
                 />
               </Grid>
-              <Grid item xs={12}>
+              <Grid item xs={12} sm={6}>
                 <TextField
                   required
                   fullWidth
                   name="password"
                   label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="confirmPassword"
+                  label="Confirm Password"
                   type="password"
                   id="password"
                   autoComplete="new-password"
@@ -127,7 +159,7 @@ export default function Signup() {
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="/login" variant="body2">
-                  Already have an account? Sign in
+                  Already have an account? Sign in!
                 </Link>
               </Grid>
             </Grid>
