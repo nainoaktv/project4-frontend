0e0a80e0176f95eaf02238cdb008ce15aaeaab96
nainoktv <nainoaktvillegas@gmail.com>
2022-12-20 18:56:53 -0800

    feat: add profile temp layout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import UserFeedBox from './UserFeedBox';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 // import { Link } from 'react-router-dom';
 
 // ++ TODO: When finished with Profile component comment back in all code ++
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Profile = (props) => {
 	// const { handleLogout, user } = props;
@@ -31,11 +43,26 @@ const Profile = (props) => {
 	// 	);
 	// };
 
+
 	return (
-		<div className="text-center pt-4">
-			{/* {user ? userData : errorDiv()} */}
-			<UserFeedBox />
-		</div>
+
+		<Box sx={{ flexGrow: 1 }}>
+			<Grid container spacing={2}>
+				<Grid item xs={6} md={8}>
+					<Item><UserFeedBox /></Item>
+				</Grid>
+				<Grid item xs={6} md={4}>
+					<Item>Top Followed</Item>
+				</Grid>
+				<Grid item xs={6} md={4}>
+					<Item>Most Popular</Item>
+				</Grid>
+				<Grid item xs={6} md={8}>
+					<Item>All Your Posts</Item>
+				</Grid>
+			</Grid>
+		</Box>
+
 	);
 
 }
